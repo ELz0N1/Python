e0c6d03d0d3b6c02165baff5a0d9a54bfca10b21
Danil Zhukov <zhukov.danila2004@yandex.ru>
2024-10-20 21:20:21 +0700

    Added solution with generator expression

--- 2.py
+++ 2.py
@@ -2,15 +2,18 @@ import unittest
 
 
 def my_zip(list1, list2):
-    # result = []
-    # for i in range(min(len(list1), len(list2))):
-    #     result.append((list1[i], list2[i]))
+    result = []
+    for i in range(min(len(list1), len(list2))):
+        result.append((list1[i], list2[i]))
+    return result
 
+
+def my_zip_using_generator(list1, list2):
     return [(list1[i], list2[i]) for i in range(min(len(list1), len(list2)))]
 
 
 class Tests(unittest.TestCase):
-    def test(self):
+    def test_my_zip(self):
         tests = [
             ([1, 2, 3], ['a', 'b']),
             ([1, 2, 3], [4, 5, 6]),
@@ -22,6 +25,19 @@ class Tests(unittest.TestCase):
         for list1, list2 in tests:
             self.assertEqual(my_zip(list1, list2), list(zip(list1, list2)))
             self.assertEqual(my_zip(list2, list1), list(zip(list2, list1)))
+
+    def test_my_zip_using_generator(self):
+        tests = [
+            ([1, 2, 3], ['a', 'b']),
+            ([1, 2, 3], [4, 5, 6]),
+            (['c', 2, 2.5], ['5', [], ()]),
+            (['a', 4, "string", 9.0], [0]),
+            ([], [])
+        ]
+
+        for list1, list2 in tests:
+            self.assertEqual(my_zip_using_generator(list1, list2), list(zip(list1, list2)))
+            self.assertEqual(my_zip_using_generator(list2, list1), list(zip(list2, list1)))
 
 
 unittest.main()
